ee34d3165095090931c4551aaad4bcb0973860c4
adica <adic@playbuzz.com>
2018-10-12 13:08:37 +0300

    Updates

---
+++ precache-manifest.4c286f9e9576aed1feb34bdff439ddb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9272467f21e3601aa6e",
+    "url": "/react-flicker-app/static/js/runtime~main.b9272467.js"
+  },
+  {
+    "revision": "b21e44a18fc0176952b4",
+    "url": "/react-flicker-app/static/js/main.b21e44a1.chunk.js"
+  },
+  {
+    "revision": "46e04b11e479a72b6f8e",
+    "url": "/react-flicker-app/static/js/1.46e04b11.chunk.js"
+  },
+  {
+    "revision": "b21e44a18fc0176952b4",
+    "url": "/react-flicker-app/static/css/main.7b407837.chunk.css"
+  },
+  {
+    "revision": "557b9e16552e0408d368913efd0a0624",
+    "url": "/react-flicker-app/index.html"
+  }
+];
