e3c4986a74886b444896f35dcd9cbf4a5d1af9ff
adica <adic@playbuzz.com>
2018-10-24 10:05:58 +0300

    fix(placeholder): replace placeholder with image (but not the other way)

--- src/components/photos/photo.js
+++ src/components/photos/photo.js
@@ -5,22 +5,25 @@ import './photo.scss';
 export default class Photo extends React.Component {
     constructor(props) {
         super(props);
-        this.placeholderUrl = '//via.placeholder.com/400x400?text=loading';
+        this.placeholderUrl = '//via.placeholder.com/350x350?text=loading';
         this.state = {
             visibility: false
         };
         this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(e) {
-        this.setState({ visibility: e });
+    handleChange(visible) {
+        //replace placeholder with image when image enter viewport (with 100px margin)
+        if (visible) {
+            this.setState({ visibility: visible });
+        }
     }
 
     render() {
         const { photo, onImageClick } = this.props;
         return (
             <article>
-                <Observer onChange={this.handleChange} rootMargin="50px 50px 50px 50px">
+                <Observer onChange={this.handleChange} rootMargin="100px 100px 100px 100px">
                     <a
                         href="#"
                         onClick={onImageClick}
